7f133f173c94f39a9b7007739af19b073ce33826
Mahammed Anish <anishk2710@gmail.com>
2023-04-19 13:02:21 +0530

    added area program

--- programs/19th april/area.py
+++ programs/19th april/area.py
@@ -1,2 +1,12 @@
-bs = int(input())
-ht = int(input())
+opt = int(input("1.Triangle Area 2. Rectangle Area 3.Square Area"))
+if(opt == 1):
+    base = int(input("Enter base "))
+    hgt = int(input("Enter height "))
+    print("Area of Triangle:",0.5*(base)*(hgt))
+elif(opt == 2):
+    ln = int(input("Enter length: "))
+    brdt = int(input("Enter breadth: "))
+    print("Area of Rectangle:",ln*brdt)
+else:
+    side = int(input("Enter side of a square: "))
+    print("Area of square:",side*side)

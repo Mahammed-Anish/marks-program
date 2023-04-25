3c0dbc8c9ce670c114767c1de02f9d6f6c093cdd
Mahammed Anish <anishk2710@gmail.com>
2023-04-25 13:16:56 +0530

    commit

--- programs/21st april/arrays.py
+++ programs/21st april/arrays.py
@@ -1,3 +1,12 @@
 from array import *
 a = array.array('i',[1,2,3,4])
 print(type(a))
+
+
+n = int(input())
+for i in range(n):
+    for k in range(n-i-1):
+        print(" ")
+    for j in range(2*i-1):
+        print("* ")
+    print()
